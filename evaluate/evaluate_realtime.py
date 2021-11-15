f508bc9d8d519ab527b773da1e0e146a2067e17c
Fabricio Torquato <22175699+fabriciotorquato@users.noreply.github.com>
2021-11-15 10:25:26 -0300

    Add cnn files

--- evaluate/evaluate_realtime.py
+++ evaluate/evaluate_realtime.py
@@ -118,30 +118,6 @@ class VideoShow:
         self.stopped = True
 
 
-class CountsPerSec:
-    """
-    Class that tracks the number of occurrences ("counts") of an
-    arbitrary event and returns the frequency in occurrences
-    (counts) per second. The caller must increment the count.
-    """
-
-    def __init__(self):
-        self._start_time = None
-        self._num_occurrences = 0
-
-    def start(self):
-        self._start_time = datetime.datetime.now()
-        return self
-
-    def increment(self):
-        self._num_occurrences += 1
-
-    def countsPerSec(self):
-        elapsed_time = (datetime.datetime.now() -
-                        self._start_time).total_seconds()
-        return self._num_occurrences / elapsed_time if elapsed_time > 0 else 0
-
-
 def putIterationsPerSec(frame, iterations_per_sec):
     """
     Add iterations per second text to lower-left corner of a frame.
